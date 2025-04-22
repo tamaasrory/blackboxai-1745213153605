b53ce2da8e11bcb7cd73edb4310f63fb506bdd54
Tama Asrory <tamaasrory@gmail.com>
2025-04-22 22:08:38 +0700

    Add lib/data/local/secure_storage.dart 1745334517628

---
+++ lib/data/local/secure_storage.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorage {
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+
+  static const String _keyAccessToken = 'access_token';
+  static const String _keyRefreshToken = 'refresh_token';
+
+  Future<void> saveAccessToken(String token) async {
+    await _storage.write(key: _keyAccessToken, value: token);
+  }
+
+  Future<String?> getAccessToken() async {
+    return await _storage.read(key: _keyAccessToken);
+  }
+
+  Future<void> saveRefreshToken(String token) async {
+    await _storage.write(key: _keyRefreshToken, value: token);
+  }
+
+  Future<String?> getRefreshToken() async {
+    return await _storage.read(key: _keyRefreshToken);
+  }
+
+  Future<void> clearTokens() async {
+    await _storage.delete(key: _keyAccessToken);
+    await _storage.delete(key: _keyRefreshToken);
+  }
+}
